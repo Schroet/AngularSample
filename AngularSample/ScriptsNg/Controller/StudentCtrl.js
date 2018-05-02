f59ff0e92927b5c4ffcc06571a5cebfb17fc4aaa
shroet <velikokhatsky@gmail.com>
2018-05-02 19:26:37 +0300

    Add project files.

---
+++ AngularSample/ScriptsNg/Controller/StudentCtrl.js
@@ -0,0 +1,4 @@
+﻿app.controller('StudentCtrl', ['$scope',
+    function ($scope) {
+        $scope.Message = 'Hellow World';
+    }]);
---
+++ AngularSample/api/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AngularSample.Models;
+
+namespace AngularSample.api
+{
+    // Route  
+    [RoutePrefix("/api/Student")]
+    public class StudentController : ApiController
+    {
+        // StudentDBEntities object point 
+        StudentDBEntities dbContext = null;
+        // Constructor  
+        public StudentController()
+        {
+            // create instance of an object 
+            dbContext = new StudentDBEntities();
+        }
+        [ResponseType(typeof(tblStudent))]
+        [HttpPost]
+        public HttpResponseMessage SaveStudent(tblStudent astudent)
+        {
+            int result = 0;
+            try
+            {
+                dbContext.tblStudents.Add(astudent);
+                dbContext.SaveChanges();
+                result = 1;
+            }
+            catch (Exception e)
+            {
+
+                result = 0;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+    }
+}
